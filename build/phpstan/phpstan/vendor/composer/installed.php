6f31b7877fb1810073339eebab91349530f827ec
Juan Ignacio <ignaciojuan3610@gmail.com>
2024-12-02 11:48:06 -0300

    primer commit de dolibar para subir archivos a git

---
+++ build/phpstan/phpstan/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b63e6d4a2003405c6b0c2880a6cd8511dc9c6940',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b63e6d4a2003405c6b0c2880a6cd8511dc9c6940',
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.10.55',
+            'version' => '1.10.55.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'reference' => '9a88f9d18ddf4cf54c922fbeac16c4cb164c5949',
+            'dev_requirement' => true,
+        ),
+    ),
+);
